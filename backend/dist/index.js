504b0a2d3a704c34515f89ff2f165fa85d6afc37
Kevin S <kevin@sirbomble.com>
2024-09-08 14:29:45 +0000

    test backend rest api

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,6 +7,10 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
     res.send('Hello World!');
+    console.log(`Request received: ${req.url}`);
+});
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
--- backend/dist/index.ts
+++ backend/dist/index.ts
@@ -3,7 +3,13 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+
+    console.log(`Request received: ${req.url}`);
+})
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); }
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
