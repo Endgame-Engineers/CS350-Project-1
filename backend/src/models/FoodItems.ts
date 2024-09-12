553acb8d4df4de66eb5502da27dc65b753a752fa
Kevin S <kevin@sirbomble.com>
2024-09-12 20:55:18 +0000

    added comments to fooditems

--- backend/src/models/FoodItems.ts
+++ backend/src/models/FoodItems.ts
@@ -14,16 +14,30 @@ export interface FoodItem {
 class FoodItems {
     private client = new ConnectToDB().connect();
 
+    /**
+     * Get all food items from db
+     * @returns Promise<FoodItem[]>
+     */
     async getFoodItems(): Promise<FoodItem[]> {
         const result = await (await this.client).query('SELECT * FROM "FoodItems"');
         return result.rows;
     }
 
+    /**
+     * Get a food item by barcode
+     * @param barcode 
+     * @returns 
+     */
     async getFoodItem(barcode: string): Promise<FoodItem> {
         const result = await (await this.client).query('SELECT * FROM "FoodItems" WHERE barcode = $1', [barcode]);
         return result.rows[0];
     }
 
+    /**
+     * Add a food item to the db
+     * @param foodItem 
+     * @returns foodItem
+     */
     async addFoodItem(foodItem: FoodItem): Promise<FoodItem> {
         await (await this.client).query(
             'INSERT INTO "FoodItems" (FoodName, Barcode, protein_per_serv, carb_per_serv, fat_per_serv, grams_per_serv, calories_per_serv) VALUES ($1, $2, $3, $4, $5, $6, $7)',
@@ -33,6 +47,10 @@ class FoodItems {
         return foodItem;
     }
 
+    /**
+     * Update a food item in the db
+     * @param foodItem 
+     */
     async updateFoodItem(foodItem: FoodItem): Promise<void> {
         await (await this.client).query(
             'UPDATE "FoodItems" SET FoodName = $1, protein_per_serv = $2, carb_per_serv = $3, fat_per_serv = $4, grams_per_serv = $5, calories_per_serv = $6 WHERE Barcode = $7',
@@ -40,10 +58,17 @@ class FoodItems {
         );
     }
 
+    /**
+     * Delete a food item from the db
+     * @param barcode 
+     */
     async deleteFoodItem(barcode: string): Promise<void> {
         await (await this.client).query('DELETE FROM "FoodItems" WHERE Barcode = $1', [barcode]);
     }
 
+    /**
+     * Close the db connection
+     */
     close(): void {
         ConnectToDB.close(this.client);
     }
