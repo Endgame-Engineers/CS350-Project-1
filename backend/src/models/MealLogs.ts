023bc91258d4f9a2631f352254bcc2a8ed4d422f
Kevin S <kevin@sirbomble.com>
2024-09-16 19:28:08 +0000

    initial implementation of Meal logs, started User Stats

--- backend/src/models/MealLogs.ts
+++ backend/src/models/MealLogs.ts
@@ -1,7 +1,7 @@
 import ConnectToDB from "../utils/ConnectToDB";
 
 export interface MealLog {
-    uuid: string;
+    mealtype: string;
     dateadded: Date;
     barcode: string;
     userid: string;
@@ -21,19 +21,22 @@ class MealLogs {
     }
 
     async getMealLog(userid: number, start: Date, end: Date): Promise<any> {
-        const result =  await (await this.client).query('SELECT * FROM "MealLogs" WHERE userid = $1 AND dateadded BETWEEN $2 AND $3', [userid, start, end]);
+        const result = await (await this.client).query(
+            `SELECT * FROM "MealLogs" WHERE userid = $1 AND DATE(dateadded) BETWEEN DATE($2) AND DATE($3) ORDER BY dateadded`,
+            [userid, start, end]
+        );
         return result.rows;
     }
 
     async addMealLog(mealLog: MealLog): Promise<void> {
         await (await this.client).query(
-            'INSERT INTO "MealLogs" (uuid, dateadded, barcode, userid, servingconsumed) VALUES ($1, $2, $3, $4, $5)',
-            [mealLog.uuid, mealLog.dateadded, mealLog.barcode, mealLog.userid, mealLog.servingconsumed]
+            'INSERT INTO "MealLogs" (mealtype, dateadded, barcode, userid, servingconsumed) VALUES ($1, $2, $3, $4, $5)',
+            [mealLog.mealtype, mealLog.dateadded, mealLog.barcode, mealLog.userid, mealLog.servingconsumed]
         );
     }
 
     async updateMealLogItem(userid: number, barcode: string, servingconsumed: number, dateadded: Date): Promise<void> {
-         await (await this.client).query(
+        await (await this.client).query(
             'UPDATE "MealLogs" SET servingconsumed = $1 WHERE userid = $2 AND barcode = $3 AND dateadded = $4',
             [servingconsumed, userid, barcode, dateadded]
         );
--- backend/src/routes/UserRoute.ts
+++ backend/src/routes/UserRoute.ts
@@ -15,16 +15,39 @@ class UserRoute {
         this.router.get('/user', isAuthenticated, (req, res) => {
             res.json(req.user);
         });
+
         this.router.get('/user/logs', isAuthenticated, (req, res) => {
             const { start, end } = req.query;
             const startDate = start ? new Date(start as string) : new Date();
             const endDate = end ? new Date(end as string) : new Date();
+            const all = req.query.all === 'true';
             const user = req.user as User;
             if (user.id) {
-                MealLogs.getMealLog(user.id, startDate, endDate)
-                    .then((mealLogs) => {
-                    res.json(mealLogs);
-                });
+                if (all) {
+                    MealLogs.getMealLogs(user.id)
+                        .then((mealLogs) => {
+                            res.json(mealLogs);
+                        });
+                } else {
+                    MealLogs.getMealLog(user.id, startDate, endDate)
+                        .then((mealLogs) => {
+                            res.json(mealLogs);
+                        });
+                }
+            } else {
+                res.status(400).json({ error: 'User not authenticated' });
+            }
+        });
+
+        this.router.post('/user/logs', isAuthenticated, (req, res) => {
+            const user = req.user as User;
+
+            if (user.id) {
+                const mealLog = { ...req.body, userid: user.id };
+                MealLogs.addMealLog(mealLog)
+                    .then((mealLog) => {
+                        res.status(201).json(mealLog);
+                    });
             } else {
                 res.status(400).json({ error: 'User not authenticated' });
             }
