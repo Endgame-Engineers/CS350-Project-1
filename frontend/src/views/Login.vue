137a9f4cc54ba54b1df5745f2001fbda1e1ce174
Kevin S <kevin@sirbomble.com>
2024-09-17 18:44:28 +0000

    login draft

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+    <b-button variant="dark">Login With Google</b-button>
+    </div>
+    </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'LoginPage',
+  }
+  </script>
