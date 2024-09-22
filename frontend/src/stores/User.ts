eb13bc52d0687c696a47a87d38fc0cc5020d3a55
Kevin S <kevin@sirbomble.com>
2024-09-22 21:19:47 +0000

    Refactor User.ts: Add profilecreated property to User model

--- frontend/src/stores/User.ts
+++ frontend/src/stores/User.ts
@@ -13,6 +13,7 @@ export const useUserStore = defineStore('user', {
             providername: '',
             providerid: '',
             profilepic: '',
+            profilecreated: false,
         } as User,
         isAuthenticated: false,
     }),
@@ -32,6 +33,7 @@ export const useUserStore = defineStore('user', {
                 providername: '',
                 providerid: '',
                 profilepic: '',
+                profilecreated: false,
             } as User
         },
 
