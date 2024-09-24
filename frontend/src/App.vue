022d5e06c79ad30896f0c0e5ac672342b2ac57dc
Kevin S <kevin@sirbomble.com>
2024-09-24 17:50:19 +0000

    Refactor Home, remove barcode in favor of add button. Move logout to top

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <main class="container mt-4 pt-5 pb-5">
-    <button v-if="$route.path.toLocaleLowerCase() !== '/login' && canGoBack" @click="goBack" class="btn btn-primary position-absolute top-0 start-0 m-3">
+    <button v-if="$route.path.toLocaleLowerCase() !== '/login' && canGoBack" @click="goBack"
+      class="btn btn-primary position-absolute top-0 start-0 m-3">
       <font-awesome-icon :icon="['fas', 'arrow-left']" />
     </button>
+    <a v-if="$route.path.toLocaleLowerCase() !== '/login'" href="/api/auth/logout" class="btn btn-outline-primary me-2 position-absolute top-0 end-0 m-3" role="button">
+      <font-awesome-icon :icon="['fas', 'sign-out-alt']" />
+    </a>
     <div class="container mt-4">
       <router-view></router-view>
     </div>
   </main>
   <footer class="container-fluid fixed-bottom bg-dark">
-    <nav :class="['navbar', 'navbar-expand-lg', 'navbar-dark',{ 'pb-5': isMobile }]">
+    <nav :class="['navbar', 'navbar-expand-lg', 'navbar-dark', { 'pb-5': isMobile }]">
       <div class="container-fluid">
 
         <div class="d-flex w-100 align-items-center">
@@ -17,7 +21,8 @@
               <img src="@/assets/images/CarbioFit.svg" alt="Carbio.fit Logo" height="30">
             </a>
           </div>
-          <div v-if="$route.path.toLocaleLowerCase() !== '/login'" class="d-flex justify-content-center justify-content-lg-end w-100">
+          <div v-if="$route.path.toLocaleLowerCase() !== '/login'"
+            class="d-flex justify-content-center justify-content-lg-end w-100">
             <router-link to="/" class="btn btn-outline-primary me-2" type="button">
               <font-awesome-icon :icon="['fas', 'house']" />
             </router-link>
@@ -27,12 +32,6 @@
             <router-link to="/profile" class="btn btn-outline-primary me-2" type="button">
               <font-awesome-icon :icon="['fas', 'user']" />
             </router-link>
-            <router-link to="/search" class="btn btn-outline-primary me-2" type="button">
-              <font-awesome-icon :icon="['fas', 'gear']" />
-            </router-link>
-            <a href="/api/auth/logout" class="btn btn-outline-primary me-2" role="button">
-              <font-awesome-icon :icon="['fas', 'sign-out-alt']" />
-            </a>
           </div>
         </div>
       </div>
