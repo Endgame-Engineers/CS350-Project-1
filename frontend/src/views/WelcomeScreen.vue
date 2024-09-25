3d6fda32a272758029baeca0d4c202d7155a64f7
default <perc8517@bears.unco.edu>
2024-09-25 17:55:20 +0000

    Removed extra form containers that werent needed Added a back button Made a slight change to the "handleSaveAndNext" method to make it perform faster

--- frontend/src/views/WelcomeScreen.vue
+++ frontend/src/views/WelcomeScreen.vue
@@ -14,33 +14,33 @@
             <h1>Enter current Weight</h1>
             <label for="weight" class="form-label">Weight</label>
             <input type="number" placeholder="Enter weight in pounds" id="weight" v-model="userStats.weight">
+            <button class="btn btn-outline-primary me-2" @click="prevStep"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
             <button class="btn btn-outline-primary me-2" type="submit"><font-awesome-icon :icon="['fas', 'arrow-right']" /></button>
         </div>
-    </form>
-    <form @submit.prevent="handleSaveAndNext">
+    
         <div v-if="step === 3" class="col-12 mb-3">
             <h1>Enter current Height</h1>
             <label for="height" class="form-label">Height</label>
             <input type="number" placeholder="Enter height in inches" id="height" v-model="userStats.height">
+            <button class="btn btn-outline-primary me-2" @click="prevStep"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
             <button class="btn btn-outline-primary me-2" type="submit"><font-awesome-icon :icon="['fas', 'arrow-right']" /></button>
         </div>
-    </form>
-    <form @submit.prevent="handleSaveAndNext">
+    
         <div v-if="step === 4" class="col-12 mb-3">
             <h1>Enter current Age</h1>
             <label for="age" class="form-label">Age</label>
             <input type="number" placeholder="Enter your age" id="age" v-model="userStats.age">
+            <button class="btn btn-outline-primary me-2" @click="prevStep"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
             <button class="btn btn-outline-primary me-2" type="submit"><font-awesome-icon :icon="['fas', 'arrow-right']" /></button>
         </div>
-    </form>
-    <form @submit.prevent="handleSaveAndNext">
+    
         <div v-if="step === 5" class="col-12 mb-3">
             <label for="dob" class="form-label">Date of Birth</label>
             <input type="date" placeholder="Enter date of birth" class="form-control" id="dob" v-model="formattedDateOfBirth">
+            <button class="btn btn-outline-primary me-2" @click="prevStep"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
             <button class="btn btn-outline-primary me-2" type="submit"><font-awesome-icon :icon="['fas', 'arrow-right']" /></button>
         </div>
-    </form>
-    <form @submit.prevent="handleSaveAndNext">
+    
         <div v-if="step === 6" class="col-12 mb-3">
             <h1>Select Activity level</h1>
             <label for="activityLevel" class="form-label">Activity Level</label>
@@ -51,16 +51,17 @@
                     <option value="4">Very Active</option>
                     <option value="5">Super Active</option>
                 </select>
+            <button class="btn btn-outline-primary me-2" @click="prevStep"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
             <button class="btn btn-outline-primary me-2" type="submit"><font-awesome-icon :icon="['fas', 'arrow-right']" /></button>
         </div>
-    </form>
-    <form>
+    
         <div v-if="step === 7" class="col-12 mb-3">
             <label for="sex" class="form-label">Sex</label>
             <select class="form-select" id="sex" v-model="userStats.sex">
                 <option value="1">Male</option>
                 <option value="2">Female</option>
             </select>
+            <button class="btn btn-outline-primary me-2" @click="prevStep"><font-awesome-icon :icon="['fas', 'arrow-left']" /></button>
             <router-link to="/">
                 <button class="btn btn-primary" type="button">Finish and Submit</button>
             </router-link>
@@ -78,7 +79,6 @@ import { addUserStats, getUserStats } from '@/services/UserStats';
 export default defineComponent({
     name: 'WelcomeScreen',
     setup(){
-        console.log("hello world")
 
         const userStore = useUserStore();
         const user = userStore.user;
@@ -97,6 +97,10 @@ export default defineComponent({
 
         const nextStep = () => {
             step.value += 1;
+        }
+
+        const prevStep = () => {
+            step.value -= 1;
         }
 
         const fetchUserStats = async () => {
@@ -121,7 +125,7 @@ export default defineComponent({
         };
 
         const handleSaveAndNext = async () => {
-            await saveUserStats();
+            saveUserStats();
             nextStep();
         };
 
@@ -151,6 +155,7 @@ export default defineComponent({
             user,
             userStats,
             nextStep,
+            prevStep,
             fetchUserStats,
             saveUserStats,
             handleSaveAndNext,
