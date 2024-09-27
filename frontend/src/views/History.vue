0d9f1bf9d89b60713478460deb9cc79cdc2d7460
Kevin S <kevin@sirbomble.com>
2024-09-27 01:22:57 +0000

    Refactor getFoodItem method to handle missing image in FoodItemsRoute.ts and allow call of bulk barcodes

--- frontend/src/views/History.vue
+++ frontend/src/views/History.vue
@@ -5,15 +5,15 @@
       <template v-for="item in mealLogs" :key="item.barcode + item.dateadded">
         <div v-if="item.mealtype.toLowerCase().includes(mealType.toLowerCase())" class="card mb-3">
           <div class="card-body">
-            <img :src="item.image" alt="food image" class="img-thumbnail">
+            <img :src="item.foodItem.image" alt="food image" class="img-thumbnail">
             <h5 class="card-title">
-              {{ item.foodname }}
+              {{ item.foodItem.foodname }}
             </h5>
             <h5 class="card-title">
               {{ item.dateadded }}
             </h5>
             <p class="card-text">
-              {{ item.servingconsumed }} servings of {{ item.foodname }}
+              {{ item.servingconsumed }} servings of {{ item.foodItem.barcode }}
             </p>
           </div>
         </div>
@@ -26,17 +26,15 @@
 import { defineComponent } from 'vue';
 import axios from 'axios';
 import { FoodItem, MealLog } from '@/models/Models';
-import { barcodeLookup } from '@/services/foodSearch';
 
 export default defineComponent({
   name: 'HistoryPage',
   data() {
-    interface HistoryData extends MealLog {
-      foodname: string;
-      image: string;
+    interface ExtendedMealLog extends MealLog {
+      foodItem: FoodItem;
     }
     return {
-      mealLogs: [] as HistoryData[],
+      mealLogs: [] as ExtendedMealLog[],
       newMeal: {
         barcode: '',
         servingconsumed: 1,
@@ -47,47 +45,10 @@ export default defineComponent({
   },
   async created() {
     const response = await axios.get('/api/user/logs?all=true');
-    this.mealLogs = await Promise.all(
-        response.data.map(async (log: MealLog) => {
-          const foodDetails = await barcodeLookup(log.barcode) as FoodItem;
-          return {
-            ...log,
-            dateadded: new Date(log.dateadded).toLocaleString(),
-            foodname: foodDetails.foodname,
-            image: foodDetails.image
-          };
-        })
-      );
+    this.mealLogs = await Promise.all(response.data);
     console.log(this.mealLogs);
   },
   methods: {
-    async addMealLog() {
-      try {
-        this.newMeal.dateadded = new Date().toISOString();
-
-        const response = await axios.post('/api/user/logs', {
-          barcode: this.newMeal.barcode,
-          servingconsumed: this.newMeal.servingconsumed,
-          mealtype: this.newMeal.mealtype,
-        });
-
-        this.mealLogs.push({
-          ...this.newMeal,
-          dateadded: new Date(this.newMeal.dateadded).toLocaleString(),
-          foodname: '',
-          image: ''
-        });
-
-        this.newMeal = {
-          barcode: '',
-          servingconsumed: 1,
-          mealtype: 'Breakfast',
-          dateadded: ''
-        };
-      } catch (error) {
-        console.error('Error adding meal log:', error);
-      }
-    }
   }
 });
 </script>
