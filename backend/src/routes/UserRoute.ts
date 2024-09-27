eb420fae301c5193d6959c0eba0b577c5098ee12
Kevin S <kevin@sirbomble.com>
2024-09-27 01:22:57 +0000

    Refactor getFoodItem method to handle missing image in FoodItemsRoute.ts and allow call of bulk barcodes

--- backend/src/routes/UserRoute.ts
+++ backend/src/routes/UserRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { User } from '../models/Users';
-import MealLogs from '../models/MealLogs';
+import MealLogs, { MealLog } from '../models/MealLogs';
 import UserStats, { UserStat } from '../models/UserStats';
 import { isAuthenticated } from '../utils/AuthGoogle';
 import { CalculateUserStats } from '../utils/CalculateUserStats';
@@ -62,21 +62,6 @@ class UserRoute {
             res.status(400).json({ error: 'User not authenticated' });
             }
         });
-
-        this.router.post('/user/stats/caloriegoal', isAuthenticated, (req, res) => {
-            // TODO: restructure this ;-;
-            const user = req.user as User;
-            const userStat = { ...req.body, updatedon: new Date() } as UserStat;
-            const calculatedStats = new CalculateUserStats(userStat);
-            const caloriegoal = calculatedStats.calculateCalorieGoal();
-
-            if (user.id) {
-                res.status(201).json({ caloriegoal });
-            } else {
-            res.status(400).json({ error: 'User not authenticated' });
-            }
-        });
-
 
         this.router.get('/user/logs', isAuthenticated, (req, res) => {
             const { start, end } = req.query;
