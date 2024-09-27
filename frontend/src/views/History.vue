b4af59182b5bfa710b9f0676898e14ba8d668b31
Kevin S <kevin@sirbomble.com>
2024-09-27 13:11:24 +0000

    Refactor router configuration: Remove Diary route

--- frontend/src/views/History.vue
+++ frontend/src/views/History.vue
@@ -1,26 +1,3 @@
-<template>
-  <div class="row">
-    <div v-for="mealType in ['Breakfast', 'Lunch', 'Dinner']" :key="mealType" class="col-12 col-md-4 mb-3">
-      <h2 class="text-center">{{ mealType }}</h2>
-      <template v-for="item in mealLogs" :key="item.barcode + item.dateadded">
-        <div v-if="item.mealtype.toLowerCase().includes(mealType.toLowerCase())" class="card mb-3">
-          <div class="card-body">
-            <img :src="item.foodItem.image" alt="food image" class="img-thumbnail">
-            <h5 class="card-title">
-              {{ item.foodItem.foodname }}
-            </h5>
-            <h5 class="card-title">
-              {{ item.dateadded }}
-            </h5>
-            <p class="card-text">
-              {{ item.servingconsumed }} servings of {{ item.foodItem.barcode }}
-            </p>
-          </div>
-        </div>
-      </template>
-    </div>
-  </div>
-</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -52,3 +29,27 @@ export default defineComponent({
   }
 });
 </script>
+
+<template>
+  <div class="row">
+    <div v-for="mealType in ['Breakfast', 'Lunch', 'Dinner']" :key="mealType" class="col-12 col-md-4 mb-3">
+      <h2 class="text-center">{{ mealType }}</h2>
+      <template v-for="item in mealLogs" :key="item.barcode + item.dateadded">
+        <div v-if="item.mealtype.toLowerCase().includes(mealType.toLowerCase())" class="card mb-3">
+          <div class="card-body">
+            <img :src="item.foodItem.image" alt="food image" class="img-thumbnail">
+            <h5 class="card-title">
+              {{ item.foodItem.foodname }}
+            </h5>
+            <h5 class="card-title">
+              {{ item.dateadded }}
+            </h5>
+            <p class="card-text">
+              {{ item.servingconsumed }} servings of {{ item.foodItem.barcode }}
+            </p>
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
