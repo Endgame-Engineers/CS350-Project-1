8bc02f2dce2b3eef5453e201bc1781b2804ce210
Kevin S <kevin@sirbomble.com>
2024-10-04 17:53:00 +0000

    Add rolling log file to backend

--- backend/src/utils/Logging.ts
+++ backend/src/utils/Logging.ts
@@ -1,2 +1,15 @@
 import { Logger, ILogObj } from "tslog";
-export const logger: Logger<ILogObj> = new Logger();
+import { createStream } from "rotating-file-stream";
+
+const stream = createStream("tslog.log", {
+    size: "10M", // rotate every 10 MegaBytes written
+    interval: "1d", // rotate daily
+    compress: "gzip", // compress rotated files
+  });
+
+const logger: Logger<ILogObj> = new Logger();
+logger.attachTransport((logObj) => {
+    stream.write(JSON.stringify(logObj) + "\n");
+  });
+
+export { logger };
