f4ee053790c242fce0697729bb1f2a103cda8e0b
Kevin S <kevin@sirbomble.com>
2024-10-04 18:51:09 +0000

    Updated logging for prod.

--- backend/src/utils/Logging.ts
+++ backend/src/utils/Logging.ts
@@ -1,7 +1,13 @@
 import { Logger, ILogObj } from "tslog";
 import { createStream } from "rotating-file-stream";
+import { config } from "dotenv";
 
-const stream = createStream("tslog.log", {
+config();
+
+// Get log file path from environment variable
+const logFileDir = process.env.LOG_FILE_PATH;
+
+const stream = createStream(logFileDir ? `${logFileDir}/app.log` : "app.log", {
     size: "10M", // rotate every 10 MegaBytes written
     interval: "1d", // rotate daily
     compress: "gzip", // compress rotated files
