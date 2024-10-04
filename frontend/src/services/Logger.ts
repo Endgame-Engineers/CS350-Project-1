ac48f69453849911a2d00b9761758e7c7650e693
Kevin S <kevin@sirbomble.com>
2024-10-04 17:56:45 +0000

    Wait you can add files to frontend lol

--- frontend/src/services/Logger.ts
+++ frontend/src/services/Logger.ts
@@ -1,15 +1,2 @@
 import { Logger, ILogObj } from "tslog";
-import { createStream } from "rotating-file-stream";
-
-const stream = createStream("tslog.log", {
-    size: "10M", // rotate every 10 MegaBytes written
-    interval: "1d", // rotate daily
-    compress: "gzip", // compress rotated files
-  });
-
-const logger: Logger<ILogObj> = new Logger();
-logger.attachTransport((logObj) => {
-    stream._write(JSON.stringify(logObj) + "\n", 'utf8', () => {});
-  });
-
-export { logger };
+export const logger: Logger<ILogObj> = new Logger();
