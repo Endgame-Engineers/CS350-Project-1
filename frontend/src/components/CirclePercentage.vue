c6f2dc9d787f9d83c48c11e135aaaa11345cc4b9
Kevin S <kevin@sirbomble.com>
2024-11-21 15:47:02 +0000

    Revert "SCRUM-24 Refactor CirclePercentage component to remove subtitle and handle NaN values; update Logs view for improved layout and formatting"
    This reverts commit b5c4877430e7cdb8953296557efb223fce11d3d0.

--- frontend/src/components/CirclePercentage.vue
+++ frontend/src/components/CirclePercentage.vue
@@ -7,7 +7,7 @@
            a 15.9155 15.9155 0 0 1 0 -31.831" />
             <!-- Progress circle -->
             <text v-if="props.subtitle" x="18" y="26" class="circle-subtitle">{{ props.subtitle }}</text>
-            <path v-if="props.progress > 0" class="circle" :stroke-dasharray="props.progress + ', 100'" d="M18 2.0845
+            <path v-if="Number(props.progress) > 0" class="circle" :stroke-dasharray="props.progress + ', 100'" d="M18 2.0845
            a 15.9155 15.9155 0 0 1 0 31.831
            a 15.9155 15.9155 0 0 1 0 -31.831" />
             <text x="18" y="18" class="percentage">{{ computedProgress }}%</text>
@@ -26,5 +26,5 @@ const props = defineProps<{
     subtitle?: string;
 }>();
 
-const computedProgress = computed(() => isNaN(props.progress) ? 0 : props.progress);
+const computedProgress = computed(() => isNaN(Number(props.progress)) ? 0 : Number(props.progress));
 </script>
