15ee4352a066558911d4624f5d8d863b8e5b6dbe
default <perc8517@bears.unco.edu>
2024-12-05 05:22:20 +0000

    SCRUM-34 refactor: improve BarcodeScanner class by lazy initializing codeReader and enhancing type safety

--- frontend/src/services/BarcodeScanner.ts
+++ frontend/src/services/BarcodeScanner.ts
@@ -1,18 +1,23 @@
 import { BrowserMultiFormatReader, Result } from "@zxing/library";
+
 class BarcodeScanner {
-    codeReader: BrowserMultiFormatReader;
-    constructor() {
-        this.codeReader = new BrowserMultiFormatReader();
+    private codeReader: BrowserMultiFormatReader | null = null;
+
+    private getCodeReader(): BrowserMultiFormatReader {
+        if (!this.codeReader) {
+            this.codeReader = new BrowserMultiFormatReader();
+        }
+        return this.codeReader;
     }
+
     async barcodeReader(video: HTMLVideoElement, selectedDeviceId: string | null): Promise<string | null> {
         try {
+            const codeReader = this.getCodeReader();
             return new Promise((resolve, reject) => {
-                // TODO: should fix to be type safe.
-                // eslint-disable-next-line
-                this.codeReader.decodeFromVideoDevice(selectedDeviceId, video, (result: Result | null, error: any) => {
+                codeReader.decodeFromVideoDevice(selectedDeviceId, video, (result: Result | null, error: Error | undefined) => {
                     if (result) {
                         console.log(result.getText());
-                        this.codeReader.reset();
+                        codeReader.reset();
                         resolve(result.getText());
                     } else if (error && error.message !== "No MultiFormat Readers were able to detect the code.") {
                         console.error("Error during scanning:", error);
@@ -27,7 +32,9 @@ class BarcodeScanner {
     }
 
     async closeBarcodeReader() {
-        this.codeReader.reset();
+        const codeReader = this.getCodeReader();
+        codeReader.reset();
     }
 }
+
 export default new BarcodeScanner();
